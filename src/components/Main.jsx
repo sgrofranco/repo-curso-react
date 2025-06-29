4f0d600912cd7283c4b639d8f4d0587fb42b654a
sgrofranco <franky.sgro@gmail.com>
2025-06-29 17:19:15 -0300

    fix: ajustes menores de estilo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { CarouselCaption } from 'react-bootstrap';
 
 
 const Main = ({ data }) => {
 
   return (
     <main>
-      <Carousel interval={3000} pause={false} controls={true} indicators={true}>
+      <div class="carousel-caption-custom">
+        <img class="logo-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Eo_circle_deep-orange_letter-t.svg/1024px-Eo_circle_deep-orange_letter-t.svg.png" style={{width:"250px"}} />
+        <h1>TECHNOSTORE</h1>
+      </div>
+      <Carousel interval={3000} pause={false} controls={true} indicators={true} className='carousel-custom'>
         <Carousel.Item>
           <img
             className="d-block w-100"
