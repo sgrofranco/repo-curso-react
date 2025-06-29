6399fddc13872db24bffd8de0053ff11e1effc4e
sgrofranco <franky.sgro@gmail.com>
2025-06-29 01:06:43 -0300

    Feat: buscador de productos

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,15 +2,31 @@ import React from 'react'
 import Product from './Product'
 import './ProductList.css'
 
-const ProductList = ({products,addToCart}) => {
+const ProductList = ({ products, addToCart }) => {
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const filteredProducts = products.filter((product) =>
+    product.nombre.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
+    <div className="product-list-container">
+      <input
+        type="text"
+        placeholder="Buscar productos..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="search-input"
+      />
     <div className="product-list">
-    {products.map((product) => (
-      <div key={product.id} className="product-card">
-        <Product product={product} addToCart={addToCart} />
+        {filteredProducts.map((product) => (
+          <div key={product.id} className="product-card">
+            <Product product={product} addToCart={addToCart} />
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
+    </div>
   )
 }
 
